541ed88d812d8930ce93df256b2252f92372e45d
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-01-15 09:35:54 +0900

    assert문 추가

--- tests/test_first.py
+++ tests/test_first.py
@@ -4,11 +4,16 @@ def test_first():
     v = get_os_pretty_name()
     assert v is not None
     assert v == "Ubuntu 24.04.1 LTS"
-# 빈문자열이 아닌지
-# 문자열에 LTS가 포함되었는지
-# 문자열에 문자도 있고, 숫자도 있는지
-# . 이 포함되어있는지
-# 기타 등등 ...
-# 길이가 적어도 얼마 이상인지...
-
+    # 빈문자열이 아닌지
+    assert v != ""
+    # 문자열에 LTS가 포함되었는지
+    assert "LTS" in v
+    # 문자열에 문자도 있고, 숫자도 있는지
+    assert any(c.isalpha() for c in v)
+    assert any(c.isdigit() for c in v)
+    # . 이 포함되어있는지
+    assert "." in v
+    # 길이가 적어도 7 이상인지 확인
+    assert len(v) >= 7
+    # 기타 등등 ...
 
